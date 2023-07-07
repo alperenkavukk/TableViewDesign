211da853b75a7a4f09198696808e38d23c8e2793
alperenkavukk <kavuk.67.76@gmail.com>
2023-07-07 16:05:56 +0300

    a

---
+++ TableViewDesign/TableViewCell.swift
@@ -0,0 +1,54 @@
+//
+//  TableViewCell.swift
+//  TableViewDesign
+//
+//  Created by Alperen Kavuk on 7.07.2023.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var izinSayısıLbl: UILabel!
+    @IBOutlet weak var meslekLbl: UILabel!
+    @IBOutlet weak var yearlbl: UILabel!
+    @IBOutlet weak var cinsiyetLbl: UILabel!
+    @IBOutlet weak var adSoyadLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        adSoyadLbl.numberOfLines = 0
+        cinsiyetLbl.numberOfLines = 0
+        yearlbl.numberOfLines = 0
+        meslekLbl.numberOfLines = 0
+        izinSayısıLbl.numberOfLines = 0
+            
+                
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .top
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        
+               
+        stackView.addArrangedSubview(adSoyadLbl)
+        stackView.addArrangedSubview(cinsiyetLbl)
+        stackView.addArrangedSubview(yearlbl)
+        stackView.addArrangedSubview(meslekLbl)
+        stackView.addArrangedSubview(izinSayısıLbl)
+        contentView.addSubview(stackView)
+                
+               
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+            
+            //adSoyadLbl.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: 0.5, constant: -10)
+                ])
+            }
+    }
+
--- TableViewDesign/ViewController.swift
+++ TableViewDesign/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import UIKit
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    var isimArray = ["AlperenKavukKavuk","Alperen","alper","Alperen Kavuk","Alperen Kavuk Alperen"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+      
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+       
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 50
+        
+       
+        tableView.reloadData()
     }
-
-
+    
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+   
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+        
+        cell.adSoyadLbl.text = isimArray[indexPath.row]
+        cell.yearlbl.text = "Yaş:30"
+        cell.cinsiyetLbl.text =  "Erkek"
+        cell.izinSayısıLbl.text = "İzinS:30"
+        cell.meslekLbl.text = "Yazılım"
+       
+        
+        
+        return cell
+    }
 }
 
